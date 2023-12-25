08ff34010d3fa9cc94de0e773b03082324f7228d
Ahmad Amin <baigahmad323@gmail.com>
2023-12-26 01:59:47 +0500

    Create Lobby - Join/Create Room form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+import styles from '@/styles/home.module.css'
 
 export default function Home() {
+  const router = useRouter();
+  const [roomId, setRoomId] = useState('')
+
+  const createAndJoin = () => {
+    const roomId = uuidv4()
+    router.push(`/${roomId}`)
+  }
+
+
+  const joinRoom = () => {
+    if(roomId) {
+      router.push(`/${roomId}`)
+    }
+    else{
+      alert('Please provide a valid room ID')
+    }
+  }
 
   return (
-    <h1>Welcome</h1>
+    <div className={styles.homeContainer}>
+      <h1>Google Meet Clone</h1>
+      <div className={styles.enterRoom}>
+        <input placeholder='Enter Room ID' value={roomId} onChange={(e) => setRoomId(e?.target.value)} />
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+      <span className={styles.separatorText}>-------------------- OR -------------------</span>
+      <button onClick={createAndJoin}>Create a new Room</button>
+    </div>
   )
 }
