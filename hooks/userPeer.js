17fabf6452026d2cafe48b1f31f160f4d3352eb5
Ahmad Amin <baigahmad323@gmail.com>
2023-12-26 17:03:31 +0500

    Signaling

--- hooks/userPeer.js
+++ hooks/userPeer.js
@@ -1,25 +1,29 @@
+import { useSocket } from '@/context/socket';
+import { useRouter } from 'next/router';
 import { useEffect, useState, useRef } from 'react';
-// import Peer from 'peerjs'
 
 const usePeer = () => {
+  const socket = useSocket();
+  const roomId = useRouter().query.roomId;
   const [peer, setPeer] = useState(null);
   const [myId, setMyId] = useState('');
 
   const isPeerSet = useRef(false);
 
   useEffect(() => {
-    if (isPeerSet.current) return;
+    if (isPeerSet.current || !roomId || !socket) return;
     isPeerSet.current = true;
     (async function initPeer() {
       const myPeer = new (await import('peerjs')).default();
-      // const myPeer = new Peer();
       setPeer(myPeer);
+
       myPeer.on('open', (id) => {
         console.log(`your peer id is ${id}`);
         setMyId(id);
+        socket?.emit('join-room', roomId, id )
       });
     })();
-  }, []);
+  }, [roomId, socket]);
 
   return {
     peer,
